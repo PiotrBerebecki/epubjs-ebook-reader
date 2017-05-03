200b455a449c969a2750c72957cec8267380d41e
Akin909 <akin.sowemimo@gmail.com>
2017-05-03 13:57:40 +0100

    Add react reader

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {EpubView} from 'react-reader'
+import { localforage } from 'localforage';
+import './books/moby-dick.epub';
 
-// import  from 'epubjs';
-//
-//
-// var Book = ePub('./books/moby-dick.epub', {
-//   storage: true,
-// });
+// console.log('book', book.length);
+const epubOptions = {
+    disableSrcdoc: true // If you use react-reader within cordova you need to set this because cordova webview will silently disable srcdoc for iframes, see https://issues.apache.org/jira/browse/CB-7379
+}
 
+console.log('localforage', localforage);
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+
+    this.saveBook = this.saveBook.bind(this);
+  }
+
+  saveBook(){
+    localforage.setItem('key','moby-dick' ).then(() => {
+      console.log('ran' );
+    })
+  }
+
+
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      /* The ReactReader will expand to 100% of width/height, so be sure
+       * to set a height on the parent element, either with position it
+       * absolute of window, set height or use paddingTop for
+       * proporsjonal scaling */
+      <div style={{position: 'relative', height: '100%'}}>
+        <EpubView 
+          url={'./books/moby-dick.epub'} 
+          locationChanged={(epubcifi) => console.log(epubcifi)}
+          tocChanged={(toc) => console.log(toc)}
+          epubOptions={epubOptions}
+        />
+        <button onClick={this.saveBook}></button>
       </div>
-    );
+    )
   }
 }
 
